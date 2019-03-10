150c500e82feebe7858ccce1c207176aff456e9c
Stan <stan@powwowenergy.com>
2019-03-10 11:20:58 -0700

    Fix the display issue due to missing css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// include leaflet.css so it gets into the bundle
+import 'leaflet/dist/leaflet.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
